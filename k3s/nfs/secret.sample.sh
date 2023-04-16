40d6aad6cf9778529e1f05d88afc9051d8879cb5
Lanznx <1109305092@g.nccu.edu.tw>
2023-04-16 10:48:00 +0000

    refactor: merge config and sep db-pv

--- k3s/nfs/secret.sample.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret generic {SECRET_NAME} \
---from-literal=MYSQL_USERNAME={MYSQL_USERNAME} \
---from-literal=MYSQL_PASSWORD={MYSQL_PASSWORD} \
---from-literal=MYSQL_URL={MYSQL_URL} \
---namespace={NAME_SPACE}
