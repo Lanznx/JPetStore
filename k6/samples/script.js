fc8ccb9c58aa260f48cc11d1884e11111776194c
Dt22 <82443036+Distant22@users.noreply.github.com>
2023-06-11 22:06:37 +0800

    Recreate k6 folder (#6)
    * Update dependency com.codeborne:selenide to v6.11.1
    
    * Update dependency com.codeborne:selenide to v6.11.2
    
    * Update dependency maven to v3.8.7
    
    * Update dependency org.seleniumhq.selenium:htmlunit-driver to v4.8.0
    
    * Update mockito monorepo to v5.1.1
    
    * chore(deps): update openjdk docker tag to v17.0.2
    
    * chore(deps): update dependency maven to v3.9.0
    
    * chore: add .env sample file
    
    * chore(deps): update dependency org.codehaus.cargo:cargo-maven3-plugin to v1.10.5
    
    * chore(deps): update dependency org.apache.maven.plugins:maven-failsafe-plugin to v3.0.0-m9
    
    * chore(deps): update dependency com.codeborne:selenide to v6.12.0
    
    * fix(deps): update dependency org.mybatis:mybatis to v3.5.12
    
    * chore(deps): update dependency com.codeborne:selenide to v6.12.2
    
    * chore(deps): update mockito monorepo to v5.2.0
    
    * fix(deps): update dependency org.mybatis:mybatis to v3.5.13
    
    * chore(deps): update dependency org.codehaus.cargo:cargo-maven3-plugin to v1.10.6
    
    * chore(deps): update dependency maven-wrapper to v3.2.0
    
    * chore(deps): update dependency com.codeborne:selenide to v6.12.3
    
    * chore(deps): update dependency org.apache.maven.plugins:maven-failsafe-plugin to v3.0.0
    
    * fix: rm mysql and restore to in memory database
    
    * chore: rm .env.sample
    
    * Remove submodule: k6
    
    * fix(k6):remove submodules and add k6 folder
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Jeremy Landis <jeremylandis@hotmail.com>
    Co-authored-by: Lanznx <109305092@g.nccu.edu.tw>

---
+++ k6/samples/script.js
@@ -0,0 +1,39 @@
+import { sleep, check, group } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  vus: 1000,
+  duration: '10s',
+}
+
+
+export default function Register() {
+
+  function generateRandomString(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  let response
+  const data = generateRandomString(10)
+  group('Register', function () {
+    response = http.post( 
+      'http://34.173.229.193:8080//jpetstore/actions/Account.action;',
+      `username=${data}&password=j2ee&repeatedPassword=j2ee&account.firstName=${data}&account.lastName=${data}&account.email=${data}&account.phone=${data}&account.address1=${data}&account.address2=${data}&account.city=${data}&account.state=${data}&account.zip=${data}&account.country=${data}&account.languagePreference=english&account.favouriteCategoryId=FISH&newAccount=Save Account Information&_sourcePage=oXxdIE9wLvknHfdpOfVv1_HSSP7U0TjQXKUueZ-pcEl12_lEHVTjmySBuR9aRaQkB9cwk2viOSxiuKfc8m-IwFgxcfO1dOlqFZCyM249tTA=&__fp=rvB7Mww1bCdoEMnIev_TleDqL3938m9SADN3W-ia4AF5Jc9HqbsskLB5Jvjsathjuu3sXzxWsGB6gPRLGCAYz7mAsO_etUGKgPBinwNCCwtQw_WxpkK9tqy7bzWeGQtdEWFx82zrF8Z19IQ2XtnRRWNNV0G-8ZTVErw2oYgh9uerKQnfYYTQx3ZRAWaUn90q`,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+    check(response, { 'status equals 200': response => response.status.toString() === '200' })
+  })
+
+  // Automatically added sleep
+  sleep(1)
+}
